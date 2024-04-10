51d239d2f185dc397612753d333d255c21ad33f7
Michele Cheow <ldnallen@gmail.com>
2024-04-09 20:02:53 -0700

    fixed typo in ‘apiVersion’

--- portfolio2024/sanity.config.ts
+++ portfolio2024/sanity.config.ts
@@ -6,7 +6,7 @@ const config = defineConfig({
   projectId: "883v7oiz",
   dataset: "production",
   title: "My Portfolio Site",
-  apiVerson: "2024-04-08",
+  apiVersion: "2024-04-08",
   basePath: "/admin",
   plugins: [structureTool()],
   schema: { types: schemas },
