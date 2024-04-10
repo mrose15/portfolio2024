90d28c21c1012b5d4d1b0166b5592d836210f8e6
Michele Cheow <ldnallen@gmail.com>
2024-04-09 20:01:18 -0700

    created sanity-utils file where groq query for project lives, also imported type from Project.ts

---
+++ portfolio2024/sanity/sanity-utils.ts
@@ -0,0 +1,22 @@
+import { Project } from "@/types/Project";
+import { createClient, groq } from "next-sanity";
+
+export async function getProjects(): Promise<Project[]> {
+  const client = createClient({
+    projectId: "883v7oiz",
+    dataset: "production",
+    apiVersion: "2024-04-08",
+  });
+
+  return client.fetch(
+    groq`*[_type == "project"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+        }`
+  );
+}
