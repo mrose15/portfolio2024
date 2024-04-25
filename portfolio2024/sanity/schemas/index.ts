b194787ea2ac54c87fc361b72705af2bd8200049
Michele Cheow <ldnallen@gmail.com>
2024-04-25 09:27:01 -0700

    added post type and schema files

--- portfolio2024/sanity/schemas/index.ts
+++ portfolio2024/sanity/schemas/index.ts
@@ -1,6 +1,7 @@
 import page from "./page-schema";
 import project from "./project-schema";
+import post from "./post-schema";
 
-const schemas = [project, page];
+const schemas = [project, page, post];
 
 export default schemas;
