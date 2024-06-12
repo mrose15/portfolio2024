ed2e43abaec50f66f2af8c76a7660bc5504bdcb4
Michele Cheow <ldnallen@gmail.com>
2024-06-12 08:37:47 -0700

    Added roboto condensed, started to set up global styles for headers

--- portfolio2024/app/(site)/page.tsx
+++ portfolio2024/app/(site)/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
         !
       </h1>
       <p className="mt-3 text-xl text-gray-600">Sup? Check out my projects</p>
-      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
+      <h2 className="mt-24 text-gray-700 text-3xl">Projects</h2>
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
           <Link
